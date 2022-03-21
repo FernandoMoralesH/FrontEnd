c5edcfc9ace3a57e21a7529acb832db8a2c77146
FernandoMoralesH <99518846+FernandoMoralesH@users.noreply.github.com>
2022-03-20 18:37:58 -0600

    Actualizar practica 3

---
+++ Practica 4/ap1.js
@@ -0,0 +1,8 @@
+const input =document.querySelector('input');
+const button =document.querySelector('button');
+const pokemonContainer =document.querySelector('pokemonContainer');
+
+function traerPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokeathlon-stat/{id or name}');
+    .then(res)
+}
